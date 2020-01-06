c91d713912081b36519bc8680ba36362023b6000
Albert Zhang <alchzh@gmail.com>
2020-01-06 21:32:58 +0000

    init

--- scraper.py
+++ scraper.py
@@ -1,11 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
+from markupsafe import Markup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'
 }
 
+def removeImg(soup):
+    for img in soup.find_all('img'):
+        img.decompose()
+    for img in soup.find_all(class_='icon'):
+        img.decompose()
+    for img in soup.find_all(class_='silo-group'):
+        img.decompose()
+    return soup
+
+def processInterchange(di):
+    return di.split(", ")[0][1:]
 
 def scrape(baseURL):
     results = {}
@@ -16,8 +28,9 @@ def scrape(baseURL):
     overviewSoup = BeautifulSoup(overview.text, "html.parser")
 
     speciesInfoCardContents = overviewSoup.find(class_="speciesInfoCard").contents
-    results["infoCard"] = speciesInfoCardContents[0]
-    results["basicDesc"] = speciesInfoCardContents[1].find("p")
+    results["infoCard"] = removeImg(speciesInfoCardContents[0])
+    species_info = results["infoCard"].find(class_='species-info')
+    species_info.insert(len(species_info.contents), speciesInfoCardContents[1].find("p"))
 
     results["audio"] = overviewSoup.find(id="jquery_jplayer_audio")["name"]
 
@@ -26,16 +39,20 @@ def scrape(baseURL):
     idPage = requests.get(idURL, headers=headers)
     idSoup = BeautifulSoup(idPage.text, "html.parser")
 
+    track = idSoup.find(class_="slider").contents
+    results['imgs'] = [{'annotation': Markup(slide.select('.annotation-txt > a')[0]), 'url': processInterchange(slide.find('img')['data-interchange'])} for slide in track]
+        
+
     fourKeys = idSoup.find('ul', class_="four-keys").contents
     sizeShape = fourKeys[0].find('article')
     results['sizeShape'] = sizeShape.find('p')
-    results['sizeShapeCard'] = sizeShape.find(class_='callout')
+    results['sizeShapeCard'] = removeImg(sizeShape.find(class_='callout'))
 
-    results['colorPattern'] = fourKeys[1].select('div > article > p')
-    results['behavior'] = fourKeys[2].select('div > article > p')
-    results['habitat'] = fourKeys[3].select('div > article > p')
+    results['colorPattern'] = fourKeys[1].select('div > article > p')[0]
+    results['behavior'] = fourKeys[2].select('div > article > p')[0]
+    results['habitat'] = fourKeys[3].select('div > article > p')[0]
 
-    results['regionalDifferences'] = idSoup.find_all(class_='main-column')[1].find_all('div', recursive=False)[-1].select('article > p')
+    results['regionalDifferences'] = idSoup.find_all(class_='main-column')[1].find_all('div', recursive=False)[-1].select('article > p')[0]
 
     # lifeHistory
     lhURL = urljoin(baseURL, "./lifehistory")
@@ -46,7 +63,7 @@ def scrape(baseURL):
     results['lhHabitat'] = categoryContent[0].find('p').contents[1]
     results['lhFood'] = categoryContent[1].find('p').contents[1]
     results['lhNestPlacement'] = categoryContent[2].find('p').contents[1]
-    results['lhNestDescription'] = categoryContent[2].find_all('p', recursive=False)[1].contents[1]
+    results['lhNestDescription'] = categoryContent[2].find_all('p', recursive=False)[1].contents[0]
     results['lhNestFacts'] = categoryContent[2].find('table', class_="callout")
     results['lhBehavior'] = categoryContent[3].find('p').contents[1]
     results['lhConservation'] = categoryContent[4].find('p').contents[1]
@@ -55,5 +72,12 @@ def scrape(baseURL):
 
 if __name__ == "__main__":
     import sys
+    from jinja2 import Template
 
-    print(scrape(sys.argv[1]))
+    def safeResult(soup):
+        return Markup(str(soup))
+
+    arguments = { k: safeResult(v) if isinstance(v, BeautifulSoup) else v for k, v in scrape(sys.argv[1]).items() }
+    arguments['baseURL'] = sys.argv[1]
+    print(Template(open('template.html').read()).render(arguments))
+    # print(arguments['imgs'])
