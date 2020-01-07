f4174b6f1dd15051e74f62b48fa6f565cc51f8fd
Albert Zhang <alchzh@gmail.com>
2020-01-07 01:58:34 +0000

    birds

--- scraper.py
+++ scraper.py
@@ -41,18 +41,26 @@ def scrape(baseURL):
 
     track = idSoup.find(class_="slider").contents
     results['imgs'] = [{'annotation': Markup(slide.select('.annotation-txt > a')[0]), 'url': processInterchange(slide.find('img')['data-interchange'])} for slide in track]
-        
 
-    fourKeys = idSoup.find('ul', class_="four-keys").contents
-    sizeShape = fourKeys[0].find('article')
-    results['sizeShape'] = sizeShape.find('p')
-    results['sizeShapeCard'] = removeImg(sizeShape.find(class_='callout'))
+    try:
+        fourKeys = idSoup.find('ul', class_="four-keys").contents
+        sizeShape = fourKeys[0].find('article')
+        results['sizeShape'] = sizeShape.find('p')
+        results['sizeShapeCard'] = removeImg(sizeShape.find(class_='callout'))
 
-    results['colorPattern'] = fourKeys[1].select('div > article > p')[0]
-    results['behavior'] = fourKeys[2].select('div > article > p')[0]
-    results['habitat'] = fourKeys[3].select('div > article > p')[0]
+        results['colorPattern'] = fourKeys[1].select('div > article > p')[0]
+        results['behavior'] = fourKeys[2].select('div > article > p')[0]
+        results['habitat'] = fourKeys[3].select('div > article > p')[0]
 
-    results['regionalDifferences'] = idSoup.find_all(class_='main-column')[1].find_all('div', recursive=False)[-1].select('article > p')[0]
+        results['oldIdForm'] = False
+    except AttributeError:
+        results['oldIdForm'] = True
+        results['idContent'] = removeImg(idSoup.find_all('div', class_='main-column')[-1].find('span'))
+
+    try:
+        results['regionalDifferences'] = idSoup.find_all(class_='main-column')[1].find_all('div', recursive=False)[-1].select('article > p')[0]
+    except IndexError:
+        results['regionalDifferences'] = None
 
     # lifeHistory
     lhURL = urljoin(baseURL, "./lifehistory")
@@ -62,7 +70,10 @@ def scrape(baseURL):
     categoryContent = lhSoup.find('div', class_="category-content").contents
     results['lhHabitat'] = categoryContent[0].find('p').contents[1]
     results['lhFood'] = categoryContent[1].find('p').contents[1]
-    results['lhNestPlacement'] = categoryContent[2].find('p').contents[1]
+    try:
+        results['lhNestPlacement'] = categoryContent[2].find('p').contents[1]
+    except IndexError:
+        results['lhNestPlacement'] = None
     results['lhNestDescription'] = categoryContent[2].find_all('p', recursive=False)[1].contents[0]
     results['lhNestFacts'] = categoryContent[2].find('table', class_="callout")
     results['lhBehavior'] = categoryContent[3].find('p').contents[1]
@@ -71,13 +82,28 @@ def scrape(baseURL):
     return results
 
 if __name__ == "__main__":
-    import sys
     from jinja2 import Template
 
     def safeResult(soup):
         return Markup(str(soup))
 
-    arguments = { k: safeResult(v) if isinstance(v, BeautifulSoup) else v for k, v in scrape(sys.argv[1]).items() }
-    arguments['baseURL'] = sys.argv[1]
-    print(Template(open('template.html').read()).render(arguments))
+    with open('birdlist', 'r') as f:
+        birds = [(l.split(',')[0], l.split(',')[1].strip().replace("'", '').replace(' ', '_')) for l in f.readlines()]
+
+        for (i, bird) in birds:
+            print('scraping', bird)
+            url = 'https://www.allaboutbirds.org/guide/{}/'.format(bird)
+            
+            try:
+                arguments = { k: safeResult(v) if isinstance(v, BeautifulSoup) else v for k, v in scrape(url).items() }
+                arguments['baseURL'] = url
+                output = Template(open('template.html').read()).render(arguments)
+
+                with open('birds/{:03d}_{}.html'.format(int(i), bird), 'w') as outfile:
+                    outfile.write(output)
+                    print('finished scraping', bird)
+            except AttributeError:
+                print("Error on {}", bird)
+
+
     # print(arguments['imgs'])
